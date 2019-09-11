0f13737605f69a45905995146bd2f0a40cd84810
Lena Maglo <maglo.lena@gmail.com>
2019-09-11 19:54:30 +0200

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/publications', to: 'pages#publications'
+  get '/projects', to: 'pages#projects'
+  get '/source-code', to: 'pages#source-code', as: 'source-code'
+  get '/resume', to: 'pages#resume'
+  get '/contact', to: 'pages#contact'
 end
